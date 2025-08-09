3981d0aa5f88bcd2d64fe4bf13a1e05d67bc1a7e
StruanMclean <struanmclean@mclean-dev.com>
2025-08-09 23:25:29 +0100

    Hello World

---
+++ build/CMakeFiles/StrooLang.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StrooLang.dir/src/main.c.o"
+  "CMakeFiles/StrooLang.dir/src/main.c.o.d"
+  "StrooLang"
+  "StrooLang.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StrooLang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
